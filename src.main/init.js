891096bd7d561ca8435fc43351d81e27f0803b62
Leon <leon390422914@gmail.com>
2021-01-18 10:38:54 +0800

    feat: desktop login

--- src.main/init.js
+++ src.main/init.js
@@ -4,8 +4,9 @@ const { AutoUpdate } = require('@obsidians/global')
 const CompilerManager = require('@obsidians/eth-compiler')
 const { InstanceManager } = require('@obsidians/eth-network')
 const ProjectChannel = require('@obsidians/eth-project')
+const AuthChannel = require('@obsidians/auth')
 
-let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, projectChannel
+let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, projectChannel, authChannel
 module.exports = function () {
   ipcChannel = new IpcChannel()
   keypairManager = new KeypairManager(process.env.PROJECT)
@@ -13,4 +14,5 @@ module.exports = function () {
   compilerManager = new CompilerManager()
   instanceManager = new InstanceManager()
   projectChannel = new ProjectChannel()
+  authChannel = new AuthChannel()
 }
