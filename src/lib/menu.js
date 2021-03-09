57623ad449c9a37b373af3ecbea58ad002558697
Leon <leon390422914@gmail.com>
2021-03-09 11:42:59 +0800

    feat: menu

--- src/lib/menu.js
+++ src/lib/menu.js
@@ -2,12 +2,32 @@ import get from 'lodash/get'
 
 import platform from '@obsidians/platform'
 import { globalModalManager } from '@obsidians/global'
+import { actions as projectActions } from '@obsidians/workspace'
 
 const handlers = {}
 if (platform.isDesktop) {
   const { ipcRenderer } = window.require('electron')
 
   handlers.about = () => globalModalManager.openAboutModal()
+  handlers.project = {}
+
+  const actions = [
+    'newProject',
+    'openProject',
+    'newFile',
+    'newFolder',
+    'save',
+    'saveAll',
+    'openTerminal',
+    'redo',
+    'undo',
+    'delete',
+    'selectAll',
+  ]
+
+  actions.forEach(action => {
+    handlers.project[action] = projectActions[action].bind(projectActions)
+  })
 
   ipcRenderer.on('menu-click', (event, action) => {
     const handler = get(handlers, action)
