7f1da9ccd27b8c5650d1693390e164b79e5d2244
Leon <leon390422914@gmail.com>
2021-03-09 10:15:31 +0800

    ci: devtool

--- webpack.main.prod.config.js
+++ webpack.main.prod.config.js
@@ -4,6 +4,7 @@ const baseConfig = require('./webpack.main.config')
 
 module.exports = merge.smart(baseConfig, {
   mode: 'production',
+  devtool: false,
   output: {
     path: path.resolve(__dirname, 'build', 'main')
   }
