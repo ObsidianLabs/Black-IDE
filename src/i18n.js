9ee260699e8c47c9579ffd5c0d00cf7de4e096cc
Leon <leon390422914@gmail.com>
2021-03-24 12:51:00 +0800

    feat: add i18n languageChanged listener

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import merge from 'lodash/merge'
-import { init as initI18n, resources as shareResources } from '@obsidians/i18n'
+import { init as initI18n, i18n, resources as shareResources } from '@obsidians/i18n'
 import { resources as ethResources } from '@obsidians/eth-i18n'
 
 const resources = merge(shareResources, ethResources)
@@ -10,3 +10,9 @@ initI18n(resources, {
   lng: process.env.LANG || 'en',
   debug: process.env.NODE_ENV === 'development',
 })
+
+// TODO: store language setting in localStorage, and init with this value
+// i18n.on('languageChanged', (lng) => {
+//   localStorage.setItem('language', lng)
+//   window.location.reload()
+// })
