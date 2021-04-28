bdb7b9bd2acaa70628b60a38825c68a71ab9628a
Leon <leon390422914@gmail.com>
2021-04-28 16:46:26 +0800

    bsn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 
 import { connect } from '@obsidians/redux'
+import { IpcChannel } from '@obsidians/ipc'
 
 import { networks } from '@obsidians/sdk'
 import headerActions, { Header, NavGuard } from '@obsidians/header'
@@ -9,16 +10,61 @@ import { actions } from '@obsidians/workspace'
 
 import { List } from 'immutable'
 
-const networkList = List(networks)
-
 class HeaderWithRedux extends PureComponent {
+  state = {
+    networkList: List()
+  }
+
   componentDidMount () {
     actions.history = this.props.history
     headerActions.history = this.props.history
-    if (!networkManager.network) {
-      networkManager.setNetwork(networkList.get(0))
-    }
+    this.refresh()
     this.navGuard = new NavGuard(this.props.history)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.network !== this.props.network) {
+      this.refresh()
+    }
+  }
+
+  async refresh() {
+    if (process.env.DEPLOY === 'bsn') {
+      try {
+        const ipc = new IpcChannel('bsn')
+        const projects = await ipc.invoke('projects', { chain: 'eth' })
+        console.log(projects)
+        this.setState({
+          networkList: List(projects.map(project => {
+            const url = project.endpoints?.find(endpoint => endpoint.startsWith('http'))
+            // const id =
+            return {
+              id: `bsn${project.network.id}`,
+              group: 'BSN',
+              name: `${project.network.name}`,
+              fullName: `${project.network.name} - ${project.name}`,
+              icon: 'fas fa-globe',
+              notification: `Switched to <b>${project.name}</b>.`,
+              url,
+              chainId: project.id
+            }
+          }))
+        }, this.setNetwork)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      this.setState({
+        networkList: List(networks)
+      }, this.setNetwork)
+    }
+  }
+
+  setNetwork () {
+    if (!networkManager.network) {
+      networkManager.setNetwork(this.state.networkList.get(0))
+      console.log('select', this.state.networkList.get(0))
+    }
   }
 
   groupedNetworks = networksByGroup => {
@@ -43,15 +89,17 @@ class HeaderWithRedux extends PureComponent {
 
     const selectedProject = projects.get('selected')?.toJS() || {}
 
-    const networkGroups = networkList.groupBy(n => n.group)
+    const networkGroups = this.state.networkList.groupBy(n => n.group)
     const groupedNetworks = this.groupedNetworks(networkGroups)
-    const selectedNetwork = networkList.find(n => n.id === network) || {}
+    const selectedNetwork = this.state.networkList.find(n => n.id === network) || {}
 
     const browserAccounts = uiState.get('browserAccounts') || []
     const starred = accounts.getIn([network, 'accounts'])?.toJS() || []
     const starredContracts = contracts.getIn([network, 'starred'])?.toJS() || []
     const selectedContract = contracts.getIn([network, 'selected']) || ''
     const selectedAccount = accounts.getIn([network, 'selected']) || ''
+
+    const noExplorer = process.env.DEPLOY === 'bsn'
 
     return (
       <Header
@@ -65,6 +113,7 @@ class HeaderWithRedux extends PureComponent {
         browserAccounts={browserAccounts}
         network={selectedNetwork}
         networkList={groupedNetworks}
+        // noExplorer={noExplorer}
       />
     )
   }
