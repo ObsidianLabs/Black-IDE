0f8afaef0a7b0454a8e54de7945559abea4b04d7
Leon <leon390422914@gmail.com>
2021-05-28 19:21:30 +0800

    fix: network name

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,8 +51,8 @@ class HeaderWithRedux extends PureComponent {
         return {
           id: `bsn_${project.name}${project.network.id}`,
           group: 'BSN',
-          // name: `${project.network.name}/${project.name}`,
-          name: `${project.network.name}`,
+          name: `${project.network.name}/${project.name}`,
+          // name: `${project.network.name}`,
           fullName: `${project.network.name} - ${project.name}`,
           icon: 'fas fa-globe',
           notification: `Switched to <b>${project.network.name}</b>.`,
