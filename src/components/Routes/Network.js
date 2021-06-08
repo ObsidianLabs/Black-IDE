63058c53fbce5783170c40a17f552aa3529e18e3
Phil Li <qft.gtr@gmail.com>
2021-06-07 17:04:30 -0700

    update Network tab

--- src/components/Routes/Network.js
+++ src/components/Routes/Network.js
@@ -1,32 +1,7 @@
-import React, { PureComponent } from 'react'
-import { withRouter } from 'react-router-dom'
-
-import { connect } from '@obsidians/redux'
 import Network from '@obsidians/network'
 
-class NetworkWithProps extends PureComponent {
-  state = {
-    active: true
-  }
-
-  componentDidMount () {
-    this.props.cacheLifecycles.didCache(() => this.setState({ active: false }))
-    this.props.cacheLifecycles.didRecover(() => this.setState({ active: true }))
-  }
-
-  render () {
-    return (
-      <Network
-        minerKey
-        // minerTerminal
-        networkId={this.props.network}
-        active={this.state.active}
-      />
-    )
-  }
+Network.defaultProps = {
+  minerKey: true,
 }
 
-
-export default connect([
-  'network',
-])(withRouter(NetworkWithProps))
+export default Network
