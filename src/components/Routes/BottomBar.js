d7a72f2ea334a742455e7d77255090af7bdfd3d3
Phil Li <qft.gtr@gmail.com>
2021-06-15 21:03:00 -0700

    Update BottomBar.js

--- src/components/Routes/BottomBar.js
+++ src/components/Routes/BottomBar.js
@@ -1,17 +1,1 @@
-import React from 'react'
-
-import { connect } from '@obsidians/redux'
-import BottomBar from '@obsidians/bottombar'
-
-function BottomBarWithProps ({ network, queue, uiState }) {
-  const localNetwork = uiState.get('localNetwork')
-  let txs
-  if (network !== 'dev') {
-    txs = queue.getIn([network, 'txs'])
-  } else if (localNetwork && localNetwork.lifecycle === 'started') {
-    txs = queue.getIn([localNetwork.params.id, 'txs'])
-  }
-  return <BottomBar mnemonic txs={txs} />
-}
-
-export default connect(['queue', 'network', 'uiState'])(BottomBarWithProps)
+export { default } from '@obsidians/bottombar'
