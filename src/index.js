251007ac4ce3e384112ba70c2c09b2501dcd254c
Phil Li <qft.gtr@gmail.com>
2021-06-24 13:53:24 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import '@obsidians/platform'
 import $loadjs from 'loadjs'
 
-$loadjs('https://solc-bin.ethereum.org/bin/list.js')
-
 if (!process.env.CDN) {
-  import('./react')
+  $loadjs([
+    'https://solc-bin.ethereum.org/bin/list.js',
+  ], 'bundle', {
+    async: false,
+  })
 } else {
   // Fetch files in parallel and load them in series
   $loadjs([
     'https://cdnjs.cloudflare.com/ajax/libs/react/16.12.0/umd/react.production.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.12.0/umd/react-dom.production.min.js',
+    'https://solc-bin.ethereum.org/bin/list.js',
     'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/loader.js',
     'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/editor/editor.main.js',
     'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/editor/editor.main.nls.js',
@@ -23,8 +26,8 @@ if (!process.env.CDN) {
   window.document.head.innerHTML += `<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-9ZfPnbegQSumzaE7mks2IYgHoayLtuto3AS6ieArECeaR8nCfliJVuLh/GaQ1gyM" crossorigin="anonymous">`
   window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hack-font/3.3.0/web/hack.min.css" integrity="sha512-XgCw4Srl8lC1ECwcaHwAU0WnxQwHkqmInzg9wJLtGB7DRuMaXPuK2k9WJ2AwRDGdrgK9eJpZl2hUlLi2WQssmw==" crossorigin="anonymous" />`
   window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/editor/editor.main.min.css" integrity="sha512-9uX8QlyL0SosYXO3oNqyiXdnmhtWk22wutqEzGR53Bezc+yqYVvFukBAOW97fPx/3Dxdul77zW27GwHRzdYfMg==" crossorigin="anonymous" />`
-
-  $loadjs.ready('bundle', () => {
-    import('./react')
-  })
 }
+
+$loadjs.ready('bundle', () => {
+  import('./react')
+})
