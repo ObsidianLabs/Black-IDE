7f1f51562b4139a08f8790abb5ebdcb4ae28ac44
iNorthIsle <ndobox@outlook.com>
2021-12-23 21:14:32 +0800

    feat: disable detaul context menu event

--- src/react.js
+++ src/react.js
@@ -29,6 +29,8 @@ ReactDOM.render(<App />, document.getElementById('root'))
 window.addEventListener('auxclick', (event) => {
   if (event.button === 1) event.preventDefault()
 })
+
+window.addEventListener('contextmenu', e => e.preventDefault())
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
