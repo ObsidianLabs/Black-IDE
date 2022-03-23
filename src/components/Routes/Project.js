5395bd460c07d67a443ae28950b35618d825d543
iNorthIsle <ndobox@outlook.com>
2022-03-23 09:07:14 +0800

    fix: react error layer will cover the whole screen
    Ref: https://github.com/facebook/create-react-app/issues/11773

--- src/components/Routes/Project.js
+++ src/components/Routes/Project.js
@@ -1,37 +1,39 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
 
-import platform from '@obsidians/platform'
-import { connect } from '@obsidians/redux'
-import Project from '@obsidians/eth-project'
+import Project from '@obsidians/eth-project';
+import { connect } from '@obsidians/redux';
+import platform from '@obsidians/platform';
 
 class ProjectWithProps extends PureComponent {
   async componentDidMount() {
     this.props.cacheLifecycles.didRecover(() => {
-      window.dispatchEvent(new Event('resize'))
-    })
+      window.dispatchEvent(new Event('resize'));
+    });
   }
 
   render() {
-    const { projects, uiState, match } = this.props
-    const { username, project } = match.params
-    const selected = projects.get('selected')?.toJS() || {}
+    const { projects, uiState, match } = this.props;
+    const { username, project } = match?.params;
+    const selected = projects.get('selected')?.toJS() || {};
 
-    let type, projectRoot
+    let type, projectRoot;
     if (username === 'local') {
-      type = 'Local'
-      projectRoot = selected.path
+      type = 'Local';
+      projectRoot = selected.path;
     } else {
-      type = 'Remote'
-      projectRoot = selected.id ? `${username}/${project}` : undefined
+      type = 'Remote';
+      projectRoot = selected.id ? `${username}/${project}` : undefined;
     }
 
-    return type === 'Local' && platform.isWeb ? null : <Project
-      theme='obsidians'
-      projectRoot={projectRoot}
-      type={type}
-      signer={uiState.get('signer')}
-    />
+    return type === 'Local' && platform.isWeb ? null : (
+      <Project
+        theme="obsidians"
+        projectRoot={projectRoot}
+        type={type}
+        signer={uiState.get('signer')}
+      />
+    );
   }
 }
 
-export default connect(['uiState', 'projects'])(ProjectWithProps)
+export default connect(['uiState', 'projects'])(ProjectWithProps);
