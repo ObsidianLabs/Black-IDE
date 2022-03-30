b9fad1fc4adaa35e3a71fd2e131592a07cdf2d28
captain-tab <d491405879@gmail.com>
2022-03-30 20:05:17 +0800

    fix: update env, reduxApp and userhomepage

--- src/components/Routes/UserHomepage.js
+++ src/components/Routes/UserHomepage.js
@@ -1,37 +1,43 @@
-import { BaseUserHomepage } from '@obsidians/user'
-import { connect } from '@obsidians/redux'
-import { networkManager } from '@obsidians/network'
+import { BaseUserHomepage } from '@obsidians/user';
+import { connect } from '@obsidians/redux';
+import { networkManager } from '@obsidians/network';
+
+BaseUserHomepage.defaultProps = {
+  enableTutorial: true,
+};
 
 class UserHomepage extends BaseUserHomepage {
-  componentDidMount (props) {
-    super.componentDidMount(props)
+  componentDidMount(props) {
+    super.componentDidMount(props);
     if (process.env.DEPLOY === 'bsn') {
-      this.updateNetwork()
+      this.updateNetwork();
     }
   }
 
-  async updateNetwork () {
+  async updateNetwork() {
     const urlParams = new URLSearchParams(this.props.location.search);
-    const projectId = urlParams.get('projectId')
+    const projectId = urlParams.get('projectId');
     if (!projectId) {
-      return
+      return;
     }
-    const networkId = `bsn_${urlParams.get('projectId')}`
-    this.setNetwork(networkId)
+    const networkId = `bsn_${urlParams.get('projectId')}`;
+    this.setNetwork(networkId);
   }
 
-  async setNetwork (networkId, counter = 0) {
+  async setNetwork(networkId, counter = 0) {
     if (networkManager?.networks?.length) {
-      const network = networkManager.networks.find(network => network.id === networkId)
+      const network = networkManager.networks.find(
+        (network) => network.id === networkId
+      );
       if (network) {
-        networkManager.setNetwork(network, { redirect: false, notify: true })
+        networkManager.setNetwork(network, { redirect: false, notify: true });
       }
-      this.props.history.replace(`/${this.props.match.params.username}`)
+      this.props.history.replace(`/${this.props.match.params.username}`);
     } else if (counter <= 30) {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      await this.setNetwork(networkId, ++counter)
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await this.setNetwork(networkId, ++counter);
     }
   }
 }
 
-export default connect(['profile', 'projects'])(UserHomepage)
+export default connect(['profile', 'projects'])(UserHomepage);
