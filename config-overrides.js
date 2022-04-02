19e3dac78adaad07dea612750bde243f47235932
iNorthIsle <ndobox@outlook.com>
2022-04-02 13:49:11 +0800

    chore: stringfy env

--- config-overrides.js
+++ config-overrides.js
@@ -136,7 +136,7 @@ const overrides = [
     RENDER_LOGO: JSON.stringify(process.env.RENDER_LOGO || false),
     BUILD_ID: process.env.BUILD_ID,
     COMMIT_ID: JSON.stringify(process.env.COMMIT_ID),
-    BUILD_TIME: process.env.BUILD_TIME || new Date().toString(),
+    BUILD_TIME: JSON.stringify(process.env.BUILD_TIME) || new Date().toString(),
   }),
   turnOffMangle(),
   addWasmLoader(),
