29e411532ef1d066d4ee85a10abce108645135b5
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-04-22 16:36:58 +0800

    feat: add network status redux (#50)

--- src/lib/redux/config.js
+++ src/lib/redux/config.js
@@ -1,42 +1,42 @@
-import Immutable, { List, Map } from 'immutable'
+import Immutable, { List, Map } from 'immutable';
 
-export { redux as profile } from '@obsidians/auth'
-export { redux as projects } from '@obsidians/workspace'
-export { redux as keypairs } from '@obsidians/keypair'
-export { redux as tokens } from '@obsidians/explorer'
-export { redux as abis } from '@obsidians/eth-sdk'
-export { redux as customNetworks } from '@obsidians/eth-network'
-export { redux as queue } from '@obsidians/queue'
+export { redux as profile } from '@obsidians/auth';
+export { redux as projects } from '@obsidians/workspace';
+export { redux as keypairs } from '@obsidians/keypair';
+export { redux as tokens } from '@obsidians/explorer';
+export { redux as abis } from '@obsidians/eth-sdk';
+export { redux as customNetworks } from '@obsidians/eth-network';
+export { redux as queue } from '@obsidians/queue';
 
 export const version = {
   default: Immutable.fromJS({}),
   persist: true,
   actions: {
     SET_VERSION: {
-      reducer: (state, { payload }) => state.merge(payload)
-    }
-  }
-}
+      reducer: (state, { payload }) => state.merge(payload),
+    },
+  },
+};
 
 export const globalConfig = {
   default: Immutable.fromJS({}),
   persist: true,
   actions: {
     UPDATE_GLOBAL_CONFIG: {
-      reducer: (state, { payload }) => state.mergeDeep(payload)
+      reducer: (state, { payload }) => state.mergeDeep(payload),
     },
-  }
-}
+  },
+};
 
 export const uiState = {
   default: Immutable.fromJS({}),
   persist: false,
   actions: {
     UPDATE_UI_STATE: {
-      reducer: (state, { payload }) => state.merge(payload)
+      reducer: (state, { payload }) => state.merge(payload),
     },
-  }
-}
+  },
+};
 
 export const contracts = {
   default: Immutable.fromJS({
@@ -54,20 +54,23 @@ export const contracts = {
   persist: true,
   actions: {
     CREATE_TABS: {
-      reducer: state => state
-        .updateIn(['dev', 'tabs'], (tabs = List([])) => tabs)
+      reducer: (state) =>
+        state.updateIn(['dev', 'tabs'], (tabs = List([])) => tabs),
     },
     SET_CONTRACT_TABS: {
-      reducer: (state, { payload }) => state.setIn([payload.network, 'tabs'], Immutable.fromJS(payload.tabs))
+      reducer: (state, { payload }) =>
+        state.setIn([payload.network, 'tabs'], Immutable.fromJS(payload.tabs)),
     },
     SELECT_CONTRACT: {
-      reducer: (state, { payload }) => state.setIn([payload.network, 'selected'], payload.contract)
+      reducer: (state, { payload }) =>
+        state.setIn([payload.network, 'selected'], payload.contract),
     },
     SET_STARRED_CONTRACTS: {
-      reducer: (state, { payload }) => state.setIn([payload.network, 'starred'], List(payload.starred))
+      reducer: (state, { payload }) =>
+        state.setIn([payload.network, 'starred'], List(payload.starred)),
     },
-  }
-}
+  },
+};
 
 export const accounts = {
   default: Immutable.fromJS({
@@ -82,44 +85,62 @@ export const accounts = {
       selected: '',
       accounts: [],
       tabs: [],
-    }
+    },
   }),
   persist: true,
   actions: {
     CREATE_TABS: {
-      reducer: state => state
-        .updateIn(['dev', 'tabs'], (tabs = List([])) => tabs)
+      reducer: (state) =>
+        state.updateIn(['dev', 'tabs'], (tabs = List([])) => tabs),
     },
     SET_ACCOUNT_TABS: {
-      reducer: (state, { payload }) => state.setIn([payload.network, 'tabs'], Immutable.fromJS(payload.tabs))
+      reducer: (state, { payload }) =>
+        state.setIn([payload.network, 'tabs'], Immutable.fromJS(payload.tabs)),
     },
     SET_STARRED: {
-      reducer: (state, { payload }) => state.setIn([payload.network, 'accounts'], List(payload.starred))
+      reducer: (state, { payload }) =>
+        state.setIn([payload.network, 'accounts'], List(payload.starred)),
     },
     SELECT_ACCOUNT: {
-      reducer: (state, { payload }) => state.setIn([payload.network, 'selected'], payload.account)
+      reducer: (state, { payload }) =>
+        state.setIn([payload.network, 'selected'], payload.account),
     },
     REMOVE_ACCOUNT: {
       reducer: (state, { payload }) => {
-        let index = state.getIn([payload.network, 'accounts']).indexOf(payload.account)
+        let index = state
+          .getIn([payload.network, 'accounts'])
+          .indexOf(payload.account);
         if (index === -1) {
-          return state
+          return state;
         }
-        return state.updateIn([payload.network, 'accounts'], data => data.remove(index))
-      }
+        return state.updateIn([payload.network, 'accounts'], (data) =>
+          data.remove(index)
+        );
+      },
     },
-  }
-}
+  },
+};
 
 export const network = {
   default: '',
   persist: false,
   actions: {
     SELECT_NETWORK: {
-      reducer: (_, { payload }) => payload
-    }
-  }
-}
+      reducer: (_, { payload }) => payload,
+    },
+  },
+};
+
+// TODO: merge the network Info
+export const networkConnect = {
+  default: false,
+  persist: false,
+  actions: {
+    CHANGE_NETWORK_STATUS: {
+      reducer: (_, { payload }) => payload,
+    },
+  },
+};
 
 export const chainList = {
   default: Immutable.fromJS({
@@ -128,7 +149,8 @@ export const chainList = {
   persist: false,
   actions: {
     SET_CHAIN_LIST: {
-      reducer: (state, { payload }) => state.setIn(['networks'], Immutable.fromJS(payload)),
+      reducer: (state, { payload }) =>
+        state.setIn(['networks'], Immutable.fromJS(payload)),
     },
   },
-}
+};
