7f6253cfd4ddc186d500cbdae3e4349db16b2d53
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-04-23 16:50:43 +0800

    feat: improve custom network options (#51)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,63 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
+import headerActions, { AuthModal, Header, NavGuard } from '@obsidians/header';
 
-import { connect } from '@obsidians/redux'
-import { IpcChannel } from '@obsidians/ipc'
+import { BaseProjectManager } from '@obsidians/workspace';
+import EthSdk from '@obsidians/eth-sdk';
+import { IpcChannel } from '@obsidians/ipc';
+import { List } from 'immutable';
+import { actions } from '@obsidians/workspace';
+import { connect } from '@obsidians/redux';
+import { createProject } from '../lib/bsn';
+import keypairManager from '@obsidians/keypair';
+import { networkManager } from '@obsidians/network';
 
-import headerActions, { Header, NavGuard, AuthModal } from '@obsidians/header'
-import { networkManager } from '@obsidians/network'
-import { BaseProjectManager } from '@obsidians/workspace'
-import { actions } from '@obsidians/workspace'
-import { createProject } from '../lib/bsn'
-import keypairManager from '@obsidians/keypair'
-
-import { List } from 'immutable'
-
-import EthSdk from '@obsidians/eth-sdk'
-// import BscSdk from '@obsidians/bsc-sdk'
-
-keypairManager.kp = EthSdk.kp
-networkManager.addSdk(EthSdk, EthSdk.networks)
-// networkManager.addSdk(BscSdk, BscSdk.networks)
-networkManager.addSdk(EthSdk, EthSdk.customNetworks)
+keypairManager.kp = EthSdk.kp;
+networkManager.addSdk(EthSdk, EthSdk.networks);
+networkManager.addSdk(EthSdk, EthSdk.customNetworks);
 
 class HeaderWithRedux extends PureComponent {
   state = {
-    interval: null
-  }
+    interval: null,
+  };
 
-  componentDidMount () {
-    actions.history = this.props.history
-    headerActions.history = this.props.history
-    this.refresh()
-    this.navGuard = new NavGuard(this.props.history)
+  componentDidMount() {
+    actions.history = this.props.history;
+    headerActions.history = this.props.history;
+    this.refresh();
+    this.navGuard = new NavGuard(this.props.history);
   }
 
   async refresh() {
     if (process.env.DEPLOY === 'bsn') {
-      networkManager.networks = []
-      this.getNetworks()
-      clearInterval(this.state.interval)
-      const interval = setInterval(() => this.getNetworks(), 30 * 1000)
-      this.setState({ interval })
+      networkManager.networks = [];
+      this.getNetworks();
+      clearInterval(this.state.interval);
+      const interval = setInterval(() => this.getNetworks(), 30 * 1000);
+      this.setState({ interval });
     } else {
-      this.setState({ networkList: List(networkManager.networks) }, this.setNetwork)
+      const customeNetworkMap = this.props.customNetworks.toJS();
+      const customeNetworkGroup = Object.keys(customeNetworkMap).map(
+        (name) => ({
+          group: 'others',
+          icon: 'fas fa-vial',
+          id: name,
+          name: name,
+          notification: `Switched to <b>Fantom Testnet</b>.`,
+          url: customeNetworkMap[name].url,
+        })
+      );
+      networkManager.addSdk(EthSdk, customeNetworkGroup);
     }
   }
 
-  async getNetworks () {
+  async getNetworks() {
     try {
-      const ipc = new IpcChannel('bsn')
-      const projects = await ipc.invoke('projects', { chain: 'eth' })
-      const remoteNetworks = projects.map(project => {
-        const url = project.endpoints?.find(endpoint => endpoint.startsWith('http'))
+      const ipc = new IpcChannel('bsn');
+      const projects = await ipc.invoke('projects', { chain: 'eth' });
+      const remoteNetworks = projects.map((project) => {
+        const url = project.endpoints?.find((endpoint) =>
+          endpoint.startsWith('http')
+        );
         return {
           id: `bsn_${project.id}`,
           group: 'BSN',
@@ -62,76 +70,87 @@ class HeaderWithRedux extends PureComponent {
           chainId: project.id,
           projectKey: project.key,
           symbol: 'ETH',
-          raw: project
-        }
-      })
-      networkManager.addSdk(EthSdk, remoteNetworks)
-      this.setNetwork({ redirect: false, notify: false })
+          raw: project,
+        };
+      });
+      networkManager.addSdk(EthSdk, remoteNetworks);
+      this.setNetwork({ redirect: false, notify: false });
     } catch (error) {
-      networkManager.networks = []
+      networkManager.networks = [];
     }
   }
 
-  setNetwork (options) {
+  setNetwork(options) {
     if (!networkManager.network && networkManager.networks.length) {
-      networkManager.setNetwork(networkManager.networks[0], options)
+      networkManager.setNetwork(networkManager.networks[0], options);
     }
   }
 
-  groupedNetworks = networksByGroup => {
-    const networkList = []
-    const groups = networksByGroup.toJS()
-    const keys = Object.keys(groups)
+  groupedNetworks = (networksByGroup) => {
+    const networkList = [];
+    const groups = networksByGroup.toJS();
+    const keys = Object.keys(groups);
     keys.forEach((key, index) => {
       if (key !== 'default') {
-        networkList.push({ header: key })
+        networkList.push({ header: key });
       }
-      groups[key].forEach(network => networkList.push(network))
+      groups[key].forEach((network) => networkList.push(network));
       if (index !== keys.length - 1) {
-        networkList.push({ divider: true })
+        networkList.push({ divider: true });
       }
-    })
-    return networkList
-  }
+    });
+    return networkList;
+  };
 
   setCreateProject = () => {
     const cp = async function (params) {
-      return await createProject.call(this, {
-        networkManager,
-        bsnChannel: new IpcChannel('bsn'),
-        projectChannel: BaseProjectManager.channel
-      }, params)
-    }
-    return process.env.DEPLOY === 'bsn' && cp
-  }
+      return await createProject.call(
+        this,
+        {
+          networkManager,
+          bsnChannel: new IpcChannel('bsn'),
+          projectChannel: BaseProjectManager.channel,
+        },
+        params
+      );
+    };
+    return process.env.DEPLOY === 'bsn' && cp;
+  };
 
-  renderLogo () {
+  renderLogo() {
     if (process.env.REACT_APP_LOGO) {
       return (
-        <div className='d-flex align-items-center' style={{ margin: '7px 17px' }}>
-          <img src={require(process.env.REACT_APP_LOGO).default} style={{ background: 'transparent', height: '100%' }}/>
+        <div
+          className="d-flex align-items-center"
+          style={{ margin: '7px 17px' }}
+        >
+          <img
+            src={require(process.env.REACT_APP_LOGO).default}
+            style={{ background: 'transparent', height: '100%' }}
+          />
         </div>
-      )
+      );
     }
-    return null
+    return null;
   }
 
-  render () {
-    console.debug('[render] HeaderWithRedux')
-    const { uiState, profile, projects, contracts, accounts, network } = this.props
+  render() {
+    console.debug('[render] HeaderWithRedux');
+    const { uiState, profile, projects, contracts, accounts, network } =
+      this.props;
+    const selectedProject = projects.get('selected')?.toJS() || {};
 
-    const selectedProject = projects.get('selected')?.toJS() || {}
+    const networkList = List(networkManager.networks);
+    const networkGroups = networkList.groupBy((n) => n.group);
+    const groupedNetworks = this.groupedNetworks(networkGroups);
+    const selectedNetwork = networkList.find((n) => n.id === network) || {};
 
-    const networkList = List(networkManager.networks)
-    const networkGroups = networkList.groupBy(n => n.group)
-    const groupedNetworks = this.groupedNetworks(networkGroups)
-    const selectedNetwork = networkList.find(n => n.id === network) || {}
-
-    const browserAccounts = uiState.get('browserAccounts') || []
-    const starred = accounts.getIn([network, 'accounts'])?.toJS() || []
-    const starredContracts = contracts.getIn([network, 'starred'])?.toJS() || []
-    const selectedContract = contracts.getIn([network, 'selected']) || ''
-    const selectedAccount = accounts.getIn([network, 'selected']) || ''
+    const browserAccounts = uiState.get('browserAccounts') || [];
+    const starred = accounts.getIn([network, 'accounts'])?.toJS() || [];
+    const starredContracts =
+      contracts.getIn([network, 'starred'])?.toJS() || [];
+    const selectedContract = contracts.getIn([network, 'selected']) || '';
+    const selectedAccount = accounts.getIn([network, 'selected']) || '';
 
     return (
       <Header
@@ -149,7 +168,7 @@ class HeaderWithRedux extends PureComponent {
         createProject={this.setCreateProject()}
         logo={this.renderLogo()}
       />
-    )
+    );
   }
 }
 
@@ -160,4 +179,5 @@ export default connect([
   'contracts',
   'accounts',
   'network',
-])(HeaderWithRedux)
+  'customNetworks',
+])(HeaderWithRedux);
