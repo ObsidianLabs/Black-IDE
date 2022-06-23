8415a294593447264a3fdfb75604c3a45b304286
perchless <106505412+perchless@users.noreply.github.com>
2022-06-23 16:55:29 -0700

    refactor(tailwindcss): use postcss-loader and enable JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: ['./node_modules/@obsidians/*/dist/index.es.js'],
+  content: [
+    './node_modules/@obsidians/*/dist/index.es.js',
+    './src/**/*.js',
+    './src.main/**/*.js',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-  important: '.tw-section',
 };
