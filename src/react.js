39234d58a9faf361288813706fb6b61595082791
朝天椒炒鸡蛋 <420627029@qq.com>
2022-06-27 08:34:11 +0800

    feat: create tailwind css config (#54)
    * feat: create tailwind css config
    
    * refactor(tailwindcss): use postcss-loader and enable JIT
    
    * refactor(css): generate Tailwind CSS and try to add to webpack
    
    Co-authored-by: Osmond <osmond.qkl@gmail.com>
    Co-authored-by: perchless <106505412+perchless@users.noreply.github.com>

--- src/react.js
+++ src/react.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import './scss/index.scss';
+import './scss/tailwindcss.scss';
+import './i18n';
 
-import platform from '@obsidians/platform'
-import fileOps from '@obsidians/file-ops'
+import * as serviceWorker from './serviceWorker';
 
-import './scss/index.scss'
-import './i18n'
-
-import * as serviceWorker from './serviceWorker'
-import App from './App'
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import fileOps from '@obsidians/file-ops';
+import platform from '@obsidians/platform';
 
 if (platform.isDesktop) {
-  fileOps.fsType = 'electron'
+  fileOps.fsType = 'electron';
 } else {
-  fileOps.fsType = 'web'
+  fileOps.fsType = 'web';
 }
 
 if (!process.env.CDN) {
-  import('./scss/fonts/open_sans/open_sans.css')
-  import('./scss/fonts/hack/hack.css')
-  import('./scss/fonts/fira/fira_code.css')
-  import('@fortawesome/fontawesome-pro/js/all')
+  import('./scss/fonts/open_sans/open_sans.css');
+  import('./scss/fonts/hack/hack.css');
+  import('./scss/fonts/fira/fira_code.css');
+  import('@fortawesome/fontawesome-pro/js/all');
 }
 
-document.title = process.env.PROJECT_NAME
-ReactDOM.render(<App />, document.getElementById('root'))
+document.title = process.env.PROJECT_NAME;
+ReactDOM.render(<App />, document.getElementById('root'));
 
 window.addEventListener('auxclick', (event) => {
-  if (event.button === 1) event.preventDefault()
-})
+  if (event.button === 1) event.preventDefault();
+});
 
 window.__APP_INFO__ = {
   BUILD_TIME: process.env.BUILD_TIME,
   BUILD_ID: process.env.BUILD_ID,
-  COMMIT_ID: process.env.COMMIT_ID
-}
+  COMMIT_ID: process.env.COMMIT_ID,
+};
 
-window.addEventListener('contextmenu', e => e.preventDefault())
+window.addEventListener('contextmenu', (e) => e.preventDefault());
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './node_modules/@obsidians/*/dist/index.es.js',
+    './src/**/*.js',
+    './src.main/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
