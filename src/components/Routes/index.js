ee2aaa9e3a59868bebca7d851c9aa093f5245d70
flowfire <ziyi.meng@obsidians.io>
2022-07-07 17:42:08 +0800

    feat: add welcome page

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,25 +1,37 @@
-import React, { Suspense, lazy } from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import CacheRoute, { CacheSwitch } from 'react-router-cache-route'
+import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
+import { CenterScreen, Input, LoadingScreen } from '@obsidians/ui-components';
+import React, { Suspense, lazy } from 'react';
+import { Redirect, Route } from 'react-router-dom';
 
-import Auth from '@obsidians/auth'
-import { Input, LoadingScreen, CenterScreen } from '@obsidians/ui-components'
-
-import BottomBar from './BottomBar'
+import Auth from '@obsidians/auth';
+import BottomBar from './BottomBar';
 
 Input.defaultProps = {
   type: 'text',
   autoComplete: 'off',
   autoCorrect: 'off',
   autoCapitalize: 'off',
-  spellCheck: 'false'
-}
+  spellCheck: 'false',
+};
 
-const UserHomepage = lazy(() => import('./UserHomepage' /* webpackChunkName: "Homepage" */))
-const Project = lazy(() => import('./Project' /* webpackChunkName: "Project" */))
-const Contract = lazy(() => import('./Contract' /* webpackChunkName: "Contract" */))
-const Explorer = lazy(() => import('./Explorer' /* webpackChunkName: "Explorer" */))
-const Network = lazy(() => import('./Network' /* webpackChunkName: "Network" */))
+const UserHomepage = lazy(() =>
+  import('./UserHomepage' /* webpackChunkName: "Homepage" */)
+);
+const WelcomePage = lazy(() =>
+  import('./WelcomePage' /* webpackChunkName: "WelcomePage" */)
+);
+const Project = lazy(() =>
+  import('./Project' /* webpackChunkName: "Project" */)
+);
+const Contract = lazy(() =>
+  import('./Contract' /* webpackChunkName: "Contract" */)
+);
+const Explorer = lazy(() =>
+  import('./Explorer' /* webpackChunkName: "Explorer" */)
+);
+const Network = lazy(() =>
+  import('./Network' /* webpackChunkName: "Network" */)
+);
 
 export default function (props) {
   return (
@@ -27,51 +39,51 @@ export default function (props) {
       {props.children}
       <Suspense fallback={<LoadingScreen />}>
         <CacheSwitch>
+          <Route exact path="/" render={() => <Redirect to={`/welcome`} />} />
           <Route
             exact
-            path='/'
-            render={() => <Redirect to={`/${Auth.username || 'local'}`} />}
+            path="/welcome"
+            component={WelcomePage}
+            className="p-relative w-100 h-100"
           />
           <CacheRoute
             exact
-            path='/contract/:value?'
+            path="/contract/:value?"
             component={Contract}
-            className='p-relative w-100 h-100'
+            className="p-relative w-100 h-100"
           />
           <CacheRoute
             exact
-            path='/account/:value?'
+            path="/account/:value?"
             component={Explorer}
-            className='p-relative w-100 h-100'
+            className="p-relative w-100 h-100"
           />
           <CacheRoute
             exact
-            path='/network/:network?'
+            path="/network/:network?"
             component={Network}
-            className='p-relative w-100 h-100'
+            className="p-relative w-100 h-100"
           />
           <Route
             exact
-            path='/:username'
+            path="/:username"
             component={UserHomepage}
-            className='p-relative w-100 h-100'
+            className="p-relative w-100 h-100"
           />
           <CacheRoute
             exact
-            path='/:username/:project'
-            cacheKey='project-editor'
+            path="/:username/:project"
+            cacheKey="project-editor"
             component={Project}
-            className='p-relative w-100 h-100'
+            className="p-relative w-100 h-100"
           />
-          <Route
-            render={() => <CenterScreen>Invalid URL</CenterScreen>}
-          />
+          <Route render={() => <CenterScreen>Invalid URL</CenterScreen>} />
         </CacheSwitch>
       </Suspense>
       <CacheRoute
         component={BottomBar}
-        className='border-top-1 d-flex flex-row'
+        className="border-top-1 d-flex flex-row"
       />
     </>
-  )
+  );
 }
