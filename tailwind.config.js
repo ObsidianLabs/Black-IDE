188de64b5fb8cce5d0013fcdb2be6607622cb872
perchless <106505412+perchless@users.noreply.github.com>
2022-07-27 15:55:59 +0800

    chore(config): add `Badge` related Tailwind CSS configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,24 @@ module.exports = {
     extend: {
       backgroundColor: {
         jumbotron: '#e9ecef',
+        'badge-primary': '#007bff',
+        'badge-secondary': '#6c757d',
+        'badge-success': '#28a745',
+        'badge-info': '#17a2b8',
+        'badge-warning': '#ffc107',
+        'badge-danger': '#dc3545',
+        'badge-light': '#f8f9fa',
+        'badge-dark': '#343a40',
+      },
+      colors: {
+        'badge-primary': '#fff',
+        'badge-secondary': '#fff',
+        'badge-success': '#fff',
+        'badge-info': '#fff',
+        'badge-warning': '#212529',
+        'badge-danger': '#fff',
+        'badge-light': '#212529',
+        'badge-dark': '#fff',
       },
       borderRadius: {
         1.2: '0.3rem',
