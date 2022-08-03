7966b82b4b38e7b365b8f0e73c6611e98ca13f4f
flowfire <ziyi.meng@obsidians.io>
2022-08-04 02:41:35 +0800

    fix: fix welcome page problem

--- src/components/Routes/WelcomePage.js
+++ src/components/Routes/WelcomePage.js
@@ -3,7 +3,7 @@ import '../../scss/welcomepage.scss';
 import React, { PureComponent } from 'react';
 
 import Auth from '@obsidians/auth';
-import BLACK_IDE_ICON from '../icon.png';
+// import BLACK_IDE_ICON from '../icon.png';
 import COIN_PIC from '../coin.webp';
 import DISCORD_ICON from '../discord.png';
 import ERC20_PIC from '../erc20.webp';
@@ -44,113 +44,133 @@ class WelcomePageWithProps extends PureComponent {
     return (
       <div className="welcome-page h-100">
         <div className="title">
-          <img src={BLACK_IDE_ICON} />
-          <span>Welcome to Black IDE</span>
-          {!Auth.isLogin && (
-            <div className="login-button" onClick={() => this.login()}>
-              Login
-            </div>
-          )}
-          <img
-            className={'align-right'}
-            src={DISCORD_ICON}
-            onClick={() => {
-              this.openLink('https://discord.gg/blackide');
-            }}
-          />
-          <img
-            className={'align-right'}
-            src={GITHUB_ICON}
-            onClick={() => {
-              this.openLink('https://github.com/ObsidianLabs/Black-IDE');
-            }}
-          />
-        </div>
-        <div className="desc">
-          Black IDE is an integrated development environment, making
-          developing Ethereum smart contracts faster and easier.
-        </div>
-        <div className="subtitle">Create A New Project</div>
-        <div className="desc">
-          Create an empty project, or create a project using Coin, ERC20, ERC721
-          templates.
-        </div>
-        <div className="options">
-          <div className="option" onClick={() => this.createProject()}>
-            <div className="option-title">+ New</div>
-          </div>
-        </div>
-        <div className="subtitle">What Can Black IDE Do?</div>
-        <div className="desc">
-          These main features upgrade your smart contract building experience.
-        </div>
-        <div className="options">
-          <div className="option" onClick={() => this.listProject()}>
-            <div className="option-title">Project</div>
-            <div className="option-desc">
-              Create, edit, build & deploy smart contract
+          {/* <img src={BLACK_IDE_ICON} /> */}
+          <div className="title-image"></div>
+          <div className="title-text">
+            <div className="title-h1">Welcome to Black IDE</div>
+            <div className="desc">
+              Black IDE is an integrated development environment, making
+              developing Ethereum smart contracts faster and easier.
             </div>
           </div>
-          <div
-            className="option"
-            onClick={() => this.route('/network/homestead')}
-          >
-            <div className="option-title">Network</div>
-            <div className="option-desc">
-              Deploy smart contract on popular networks
-            </div>
+          <div className="title-icons">
+            <div
+              className="title-icon icon-github"
+              onClick={() =>
+                this.openLink('https://github.com/ObsidianLabs/Black-IDE')
+              }
+            ></div>
+            <div
+              className="title-icon icon-discord"
+              onClick={() => this.openLink('https://discord.gg/blackide')}
+            ></div>
+            {!Auth.isLogin && (
+              <div className="login-button" onClick={() => this.login()}>
+                Login
+              </div>
+            )}
           </div>
-          <div className="option" onClick={() => this.route('/contract')}>
-            <div className="option-title">Contract</div>
-            <div className="option-desc">
-              Commission contract using contract inspector
-            </div>
+        </div>
+        <div className="section">
+          <div className="subtitle">Create A New Project</div>
+          <div className="desc">
+            Create an empty project, or create a project using Coin, ERC20,
+            ERC721 templates.
           </div>
-          <div className="option" onClick={() => this.route('/account')}>
-            <div className="option-title">Explorer</div>
-            <div className="option-desc">
-              Check transactions in build-in explorer
+          <div className="options">
+            <div className="option" onClick={() => this.createProject()}>
+              <div className="icon icon-create"></div>
+              <div className="option-title">Create Project</div>
             </div>
           </div>
         </div>
-        <div className="subtitle">Start With An Official Project</div>
-        <div className="desc">
-          Forking from an official project will duplicate a copy to your
-          projects, and allows you to explorer freely with an easy start.
+        <div className="section">
+          <div className="subtitle">Start With An Official Project</div>
+          <div className="desc">
+            Forking from an official project will duplicate a copy to your
+            projects, and allows you to explorer freely with an easy start.
+          </div>
+          <div className="options">
+            <div
+              className="option"
+              onClick={() => this.route('/BlackIDE-ObsidianLab/ERC-721')}
+            >
+              <div className="icon icon-sample"></div>
+              <div className="option-title">Sample ERC-721</div>
+            </div>
+            <div
+              className="option"
+              onClick={() => this.route('/BlackIDE-ObsidianLab/ERC-20')}
+            >
+              <div className="icon icon-sample"></div>
+              <div className="option-title">Sample ERC-20</div>
+            </div>
+            <div
+              className="option"
+              onClick={() => this.route('/BlackIDE-ObsidianLab/COIN')}
+            >
+              <div className="icon icon-sample"></div>
+              <div className="option-title">Sample Coin</div>
+            </div>
+          </div>
         </div>
-        <div className="options">
-          <div
-            className="option"
-            onClick={() => this.route('/BlackIDE-ObsidianLab/ERC-721')}
-          >
-            <img src={ERC721_PIC}></img>
+        <div className="section">
+          <div className="subtitle">What Can Black IDE Do?</div>
+          <div className="desc">
+            These main features upgrade your smart contract building experience.
           </div>
-          <div
-            className="option"
-            onClick={() => this.route('/BlackIDE-ObsidianLab/ERC-20')}
-          >
-            <img src={ERC20_PIC}></img>
-          </div>
-          <div
-            className="option"
-            onClick={() => this.route('/BlackIDE-ObsidianLab/COIN')}
-          >
-            <img src={COIN_PIC}></img>
+          <div className="options">
+            <div className="option" onClick={() => this.listProject()}>
+              <div className="icon icon-project"></div>
+              <div className="option-title">Project</div>
+              <div className="option-desc">
+                Create, edit, build & deploy smart contract
+              </div>
+            </div>
+            <div
+              className="option"
+              onClick={() => this.route('/network/homestead')}
+            >
+              <div className="icon icon-network"></div>
+              <div className="option-title">Network</div>
+              <div className="option-desc">
+                Deploy smart contract on popular networks
+              </div>
+            </div>
+            <div className="option" onClick={() => this.route('/contract')}>
+              <div className="icon icon-contract"></div>
+              <div className="option-title">Contract</div>
+              <div className="option-desc">
+                Commission contract using contract inspector
+              </div>
+            </div>
+            <div className="option" onClick={() => this.route('/account')}>
+              <div className="icon icon-explorer"></div>
+              <div className="option-title">Explorer</div>
+              <div className="option-desc">
+                Check transactions in build-in explorer
+              </div>
+            </div>
           </div>
         </div>
-        <div className="subtitle">Learn More About Black IDE</div>
-        <div className="desc"></div>
-        <div className="options">
-          <div
-            className="option"
-            onClick={() =>
-              this.openLink('https://github.com/ObsidianLabs/Black-IDE#readme')
-            }
-          >
-            <div className="option-title">Document</div>
-            <div className="option-desc">
-              An English/Chinese tutorial where you can learn all features of
-              Black IDE
+        <div className="section">
+          <div className="subtitle">Learn More About Black IDE</div>
+          <div className="desc"></div>
+          <div className="options">
+            <div
+              className="option"
+              onClick={() =>
+                this.openLink(
+                  'https://github.com/ObsidianLabs/Black-IDE#readme'
+                )
+              }
+            >
+              <div className="icon icon-document"></div>
+              <div className="option-title">Document</div>
+              <div className="option-desc">
+                An English/Chinese tutorial where you can learn all features of
+                Black IDE
+              </div>
             </div>
           </div>
         </div>
